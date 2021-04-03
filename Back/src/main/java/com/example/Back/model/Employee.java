4ff59f89334cd6e807aa21dabcc2af8d7a3befef
SarraSb97 <sarrasb97@gmail.com>
2021-04-03 13:04:42 +0100

    first back

---
+++ Back/src/main/java/com/example/Back/model/Employee.java
@@ -0,0 +1,66 @@
+package com.example.Back.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+@Data
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String emailId;
+
+    public Employee() {
+
+    }
+
+    public Employee(String firstName, String lastName, String emailId) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+}
